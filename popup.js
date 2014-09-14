4d7f2d91a81fc4aa7d9b847837e5b891b48efb9e
Dibyo Mukherjee <dmukherjee@apigee.com>
2014-09-14 15:20:37 -0700

    2 graphs

--- popup.js
+++ popup.js
@@ -121,7 +121,8 @@ var greenFairy = {
             modifyHtml.setNoData(host);
         }
         //MAKE GRAPHS USING SECTOR CODE
-        this.makeGraphs(results.SectorCode);
+        this.makeGraph1(results.SectorCode);
+        this.makeGraph2(results.SectorCode);
     
         // GET SECTOR INFORMATION FOR SEGMENTS
         var xmlhttp = new XMLHttpRequest();
@@ -177,7 +178,7 @@ var greenFairy = {
         }
         return components[components.length - 1]
     },
-    makeGraphs: function(sectorCode, ownScore) {
+    makeGraph1: function(sectorCode, ownScore) {
         var sector = this.sectorInfo[sectorCode];
         var aggregateSectorScore = sector.NumScores * sector.Total;
         console.log(aggregateSectorScore);
@@ -235,7 +236,64 @@ var greenFairy = {
                 data: graphData
                 }]
             });
-});
+        });
+    },
+    makeGraph2: function(sectorCode) {
+       var sector = this.sectorInfo[sectorCode];
+       var ownPercent = (this.climateCountsApi.resultData.Score *100)/this.overallTotal;
+       var otherPercent = 100 - ownPercent;
+       var graphData = [
+       {   name: "Overall",
+           y: otherPercent},
+       {
+           name: this.climateCountsApi.resultData.Company,
+           y: ownPercent,
+           selected: true
+       }
+       ];
+       console.log(graphData)
+       $(function () {
+           $('#RelTotal').highcharts({
+               chart: {
+                   plotBackgroundColor: null,
+                   plotBorderWidth: 1,//null,
+                   plotShadow: false,
+                   margin: [0, 0, 0, 0],
+                   spacingTop: 0,
+                   spacingBottom: 0,
+                   spacingLeft: 0,
+                   spacingRight: 0
+               },
+               credits: {
+                   enabled: false,
+               },
+               title: {
+                   text: ''
+               },
+               tooltip: {
+                   pointFormat: '{point.percentage:.1f}%'
+               },
+               plotOptions: {
+                   pie: {
+                       size:'100%',
+                       allowPointSelect: true,
+                       cursor: 'pointer',
+                       dataLabels: {
+                           enabled: false,
+                           format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                           style: {
+                               color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                           }
+                       }
+                   }
+               },
+               series: [{
+               type: 'pie',
+               name: 'Sector Share',
+               data: graphData
+               }]
+           });
+       }); 
     },
     // https://data.iana.org/TLD/tlds-alpha-by-domain.txt
     tldList: [ "ac","academy","accountants","active","actor","ad","ae","aero","af","ag","agency","ai","airforce","al","am","an","ao","aq","ar","archi","army","arpa","as","asia","associates","at","attorney","au","auction","audio","autos","aw","ax","axa","az","ba","bar","bargains","bayern","bb","bd","be","beer","berlin","best","bf","bg","bh","bi","bid","bike","bio","biz","bj","black","blackfriday","blue","bm","bmw","bn","bnpparibas","bo","boo","boutique","br","brussels","bs","bt","build","builders","business","buzz","bv","bw","by","bz","bzh","ca","cab","camera","camp","cancerresearch","capetown","capital","caravan","cards","care","career","careers","cash","cat","catering","cc","cd","center","ceo","cern","cf","cg","ch","cheap","christmas","church","ci","citic","city","ck","cl","claims","cleaning","click","clinic","clothing","club","cm","cn","co","codes","coffee","college","cologne","com","community","company","computer","condos","construction","consulting","contractors","cooking","cool","coop","country","cr","credit","creditcard","cruises","cu","cuisinella","cv","cw","cx","cy","cymru","cz","dad","dance","dating","day","de","deals","degree","democrat","dental","dentist","desi","diamonds","diet","digital","direct","directory","discount","dj","dk","dm","dnp","do","domains","durban","dz","eat","ec","edu","education","ee","eg","email","engineer","engineering","enterprises","equipment","er","es","esq","estate","et","eu","eus","events","exchange","expert","exposed","fail","farm","feedback","fi","finance","financial","fish","fishing","fitness","fj","fk","flights","florist","fm","fo","foo","foundation","fr","frl","frogans","fund","furniture","futbol","ga","gal","gallery","gb","gbiz","gd","ge","gent","gf","gg","gh","gi","gift","gifts","gives","gl","glass","global","globo","gm","gmail","gmo","gmx","gn","gop","gov","gp","gq","gr","graphics","gratis","green","gripe","gs","gt","gu","guide","guitars","guru","gw","gy","hamburg","haus","healthcare","help","here","hiphop","hiv","hk","hm","hn","holdings","holiday","homes","horse","host","hosting","house","how","hr","ht","hu","id","ie","il","im","immo","immobilien","in","industries","info","ing","ink","institute","insure","int","international","investments","io","iq","ir","is","it","je","jetzt","jm","jo","jobs","joburg","jp","juegos","kaufen","ke","kg","kh","ki","kim","kitchen","kiwi","km","kn","koeln","kp","kr","krd","kred","kw","ky","kz","la","lacaixa","land","lawyer","lb","lc","lease","lgbt","li","life","lighting","limited","limo","link","lk","loans","london","lotto","lr","ls","lt","ltda","lu","luxe","luxury","lv","ly","ma","maison","management","mango","market","marketing","mc","md","me","media","meet","melbourne","meme","menu","mg","mh","miami","mil","mini","mk","ml","mm","mn","mo","mobi","moda","moe","monash","mortgage","moscow","motorcycles","mov","mp","mq","mr","ms","mt","mu","museum","mv","mw","mx","my","mz","na","nagoya","name","navy","nc","ne","net","network","neustar","new","nf","ng","ngo","nhk","ni","ninja","nl","no","np","nr","nra","nrw","nu","nyc","nz","okinawa","om","ong","onl","ooo","org","organic","otsuka","ovh","pa","paris","partners","parts","pe","pf","pg","ph","pharmacy","photo","photography","photos","physio","pics","pictures","pink","pizza","pk","pl","place","plumbing","pm","pn","post","pr","praxi","press","pro","prod","productions","properties","property","ps","pt","pub","pw","py","qa","qpon","quebec","re","realtor","recipes","red","rehab","reise","reisen","ren","rentals","repair","report","republican","rest","restaurant","reviews","rich","rio","ro","rocks","rodeo","rs","rsvp","ru","ruhr","rw","ryukyu","sa","saarland","sarl","sb","sc","sca","scb","schmidt","schule","scot","sd","se","services","sexy","sg","sh","shiksha","shoes","si","singles","sj","sk","sl","sm","sn","so","social","software","sohu","solar","solutions","soy","space","spiegel","sr","st","su","supplies","supply","support","surf","surgery","suzuki","sv","sx","sy","systems","sz","tatar","tattoo","tax","tc","td","technology","tel","tf","tg","th","tienda","tips","tirol","tj","tk","tl","tm","tn","to","today","tokyo","tools","top","town","toys","tp","tr","trade","training","travel","tt","tv","tw","tz","ua","ug","uk","university","uno","uol","us","uy","uz","va","vacations","vc","ve","vegas","ventures","versicherung","vet","vg","vi","viajes","villas","vision","vlaanderen","vn","vodka","vote","voting","voto","voyage","vu","wales","wang","watch","webcam","website","wed","wf","whoswho","wien","wiki","williamhill","wme","works","ws","wtc","wtf","xn--1qqw23a","xn--3bst00m","xn--3ds443g","xn--3e0b707e","xn--45brj9c","xn--4gbrim","xn--55qw42g","xn--55qx5d","xn--6frz82g","xn--6qq986b3xl","xn--80adxhks","xn--80ao21a","xn--80asehdb","xn--80aswg","xn--90a3ac","xn--c1avg","xn--cg4bki","xn--clchc0ea0b2g2a9gcd","xn--czr694b","xn--czru2d","xn--d1acj3b","xn--fiq228c5hs","xn--fiq64b","xn--fiqs8s","xn--fiqz9s","xn--fpcrj9c3d","xn--fzc2c9e2c","xn--gecrj9c","xn--h2brj9c","xn--i1b6b1a6a2e","xn--io0a7i","xn--j1amh","xn--j6w193g","xn--kprw13d","xn--kpry57d","xn--kput3i","xn--l1acc","xn--lgbbat1ad8j","xn--mgb9awbf","xn--mgba3a4f16a","xn--mgbaam7a8h","xn--mgbab2bd","xn--mgbayh7gpa","xn--mgbbh1a71e","xn--mgbc0a9azcg","xn--mgberp4a5d4ar","xn--mgbx4cd0ab","xn--ngbc5azd","xn--nqv7f","xn--nqv7fs00ema","xn--o3cw4h","xn--ogbpf8fl","xn--p1ai","xn--pgbs0dh","xn--q9jyb4c","xn--rhqv96g","xn--s9brj9c","xn--ses554g","xn--unup4y","xn--vhquv","xn--wgbh1c","xn--wgbl6a","xn--xhq521b","xn--xkc2al3hye2a","xn--xkc2dl3a5ee0h","xn--yfro4i67o","xn--ygbi2ammx","xn--zfr164b","xxx","xyz","yachts","yandex","ye","yokohama","youtube","yt","za","zm","zone","zw" ],
@@ -433,8 +491,8 @@ var greenFairy = {
           "SectorCode": "TC",
           "Sector": "Toys & Children's Equipment"
       }
-
-  }
+    },
+  overallTotal:  6528 // From http://api.climatecounts.org/1/AggregateScores.json?Year=2011&Overall=True
 
 };
 
